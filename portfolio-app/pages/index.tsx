ea771aadcd9afce543c45ebf2d5120a05b5e15ad
nabeel ahmed <nabeel.ahmed.ceative@gmail.com>
2024-04-27 00:08:06 +0500

    second commit changes in web

---
+++ portfolio-app/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LandingPage from '@/components/modules/landingPage/landingPage'
+
+
+const LandingPagePage = () => {
+  return (
+    <LandingPage/>
+  )
+}
+
+export default LandingPagePage
